13c2fdf668bff989964220313b8890570989869a
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-03-16 21:42:27 +0100

    Clean up

--- cmake/t1Config.cmake
+++ cmake/t1Config.cmake
@@ -5,21 +5,26 @@ macro(find_test_sources OUT DIR TESTROOT PATTERN)
     file(GLOB_RECURSE ${OUT} RELATIVE "${TESTROOT}" "${DIR}/${PATTERN}")
 endmacro()
 
-macro(split_main_path MAIN NAME PATH)
-    get_filename_component(${NAME} "${MAIN}" NAME_WLE)
-    get_filename_component(${PATH} "${MAIN}" DIRECTORY)
+macro(split_path_into_filename_and_parent_path FULLPATH NAME PATH)
+    get_filename_component(${NAME} "${FULLPATH}" NAME_WLE)
+    get_filename_component(${PATH} "${FULLPATH}" DIRECTORY)
 endmacro()
 
-macro(add_test MAIN)
+macro(add_test TEST_SRC_FILE)
     set(_OPTIONS)
     set(_SINGLE_VAL_ARGS)
     set(_MULTI_VAL_ARGS INCLUDE_DIRS LIBRARIES SOURCE_DEPS)
 
+    message(VERBOSE "t1: adding test ${TEST_SRC_FILE}")
+
     cmake_parse_arguments(ADD_TEST "${_OPTIONS}" "${_SINGLE_VAL_ARGS}" "${_MULTI_VAL_ARGS}" ${ARGN})
 
-    split_main_path(${MAIN} TEST_NAME_ TEST_PATH_)
-    set(TEST_OUTPUT_DIR_ "${CMAKE_BINARY_DIR}/${TEST_PATH_}")
-    add_executable(${TEST_NAME_} ${MAIN} ${ADD_TEST_SOURCE_DEPS})
+    split_path_into_filename_and_parent_path(${TEST_SRC_FILE} TEST_NAME_ TEST_PATH_)
+    set(TEST_OUTPUT_DIR_ "${CMAKE_CURRENT_BINARY_DIR}/${TEST_PATH_}")
+
+    add_executable(${TEST_NAME_})
+
+    target_sources(${TEST_NAME_} PRIVATE ${TEST_SRC_FILE} ${ADD_TEST_SOURCE_DEPS})
 
     if (DEFINED ADD_TEST_INCLUDE_DIRS)
         target_include_directories(${TEST_NAME_} PRIVATE ${ADD_TEST_INCLUDE_DIRS})
@@ -51,7 +56,7 @@ macro(find_test_non_main_source_deps TEST_DEPS_)
     endforeach()
 endmacro()
 
-# default macro to add all .cpp files in a directory; use only if possible
+# default macro to add all .cpp files in a directory; use only if possible.
 # also adds all non-main sources of optional arguments as dependencies,
 # can get big quickly.
 # defines TEST_SOURCES
@@ -62,7 +67,7 @@ macro(add_test_directory DIR)
 
     cmake_parse_arguments(ADD_TEST_DIRECTORY "${_OPTIONS}" "${_SINGLE_VAL_ARGS}" "${_MULTI_VAL_ARGS}" ${ARGN})
 
-    message("-- t1: adding test directory ${DIR}")
+    message(STATUS "t1: adding test directory ${DIR}")
     find_test_sources(TEST_SOURCES "${DIR}" "${CMAKE_CURRENT_LIST_DIR}" "*.cpp")
     find_test_non_main_source_deps(TEST_DEPS_ ${ADD_TEST_DIRECTORY_SOURCE_DEPS})
     
@@ -84,7 +89,7 @@ macro(register_tests)
     add_custom_target(valgrindtests)
 
     foreach(EXE ${T1_TEST_EXECUTABLES})
-        split_main_path(${EXE} TEST_NAME_ TEST_PATH_)
+        split_path_into_filename_and_parent_path(${EXE} TEST_NAME_ TEST_PATH_)
         add_custom_target("run${TEST_NAME_}" COMMAND "${EXE}")
         add_dependencies(runtests "run${TEST_NAME_}")
 
